5a0febfdb4348e222b28d984f3a2f9c517c724d2
Maxime Chagnolleau <maxime.chagnolleau@free.fr>
2018-11-14 09:54:12 +0100

    add fields_config option

--- src/Form/Type/ChoiceListItemType.php
+++ src/Form/Type/ChoiceListItemType.php
@@ -16,19 +16,60 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 class ChoiceListItemType extends AbstractType
 {
     /**
+     * List of available options in fields_config
+     */
+    private const FIELDS_CONFIG_OPTIONS = [
+        'label',
+        'expanded',
+        'multiple',
+        'required',
+        'placeholder',
+        'preferred_choices',
+        'group_by',
+    ];
+
+    /**
      * @param OptionsResolver $resolver
      */
     public function configureOptions(OptionsResolver $resolver)
     {
+        // required options
         $resolver->setRequired([
             'choices_config',
         ]);
 
+        // default options
+        $resolver->setDefaults([
+            'fields_config' => [],
+        ]);
+
+        // set choices based on choices_config values
         $resolver->setDefault('choices', function (Options $options) {
             // get index
             $index = intval(str_replace(['[', ']'], '', $options['property_path']));
 
             return $options['choices_config'][$index];
+        });
+
+        // set other options values based on fields_config values
+        foreach (self::FIELDS_CONFIG_OPTIONS as $option) {
+            $this->setResolverNormalizer($resolver, $option);
+        }
+    }
+
+    /**
+     * Set values for an option based on fields_config
+     * If fields_config entry doesn't exist: keep old 'classic' value
+     * @param OptionsResolver $resolver
+     * @param string $option
+     */
+    private function setResolverNormalizer(OptionsResolver $resolver, string $option)
+    {
+        $resolver->setNormalizer($option, function (Options $options, $previous) use ($option) {
+            // get index
+            $index = intval(str_replace(['[', ']'], '', $options['property_path']));
+
+            return $options['fields_config'][$index][$option] ?? $previous;
         });
     }
 
